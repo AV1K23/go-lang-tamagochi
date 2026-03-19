5630a698cd198847d06a268b001b7be27743dcea
SANECHKA <mikilop3310@gmail.com>
2026-03-19 20:46:11 +0300

    Update main.go
    добавлено сохранение в json

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 	"time"
 )
+
+type PetStatistick struct {
+	Name   string `json:"name"`
+	Hunger int    `json:"hunger"`
+	Mood   int    `json:"mood"`
+}
 
 type Pet struct {
 	Name   string
@@ -17,6 +25,26 @@ func NewPet(name string) *Pet {
 		hunger: 50,
 		mood:   50,
 	}
+}
+
+func SavePet(p *Pet, filename string) error {
+	stats := PetStatistick{
+		Name:   p.Name,
+		Hunger: p.hunger,
+		Mood:   p.mood,
+	}
+
+	data, err := json.MarshalIndent(stats, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(filename, data, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (p *Pet) Feed() {
@@ -85,18 +113,33 @@ func (p *Pet) Online(stopChan chan bool, alertChan chan string) {
 }
 
 func main() {
-	var names string
-	fmt.Println("привет назови своего питомца")
-	fmt.Scan(&names)
+	filename := "pet_save.json"
+	var p *Pet
 
-	p := NewPet(names)
+	loadedData, err := os.ReadFile(filename)
+	if err == nil {
+		var stats PetStatistick
+		if err := json.Unmarshal(loadedData, &stats); err == nil {
+			p = &Pet{
+				Name:   stats.Name,
+				hunger: stats.Hunger,
+				mood:   stats.Mood,
+			}
+			fmt.Printf("✅ Загружен сохраненный питомец: %s\n", p.Name)
+		} else {
+			p = createNewPet()
+		}
+	} else {
+
+		p = createNewPet()
+	}
 
 	stopLife := make(chan bool)
 	alerts := make(chan string, 5)
 
 	go p.Online(stopLife, alerts)
 
-	fmt.Printf("%s живет своей жизнью...", names)
+	fmt.Printf("%s живет своей жизнью...\n\n", p.Name)
 
 	for {
 		select {
@@ -117,12 +160,26 @@ func main() {
 		case 3:
 			p.Status()
 		case 4:
+			if err := SavePet(p, filename); err != nil {
+				fmt.Println("❌ Ошибка сохранения:", err)
+			} else {
+				fmt.Println("💾 Прогресс сохранен!")
+			}
+
 			stopLife <- true
 			time.Sleep(100 * time.Millisecond)
-			fmt.Println("пока!")
+			fmt.Println("Пока!")
 			return
 		default:
 			fmt.Println("Ошибка ввода")
 		}
 	}
+}
+
+func createNewPet() *Pet {
+	var names string
+	fmt.Println("Привет! Назови своего питомца:")
+	fmt.Scan(&names)
+	fmt.Println("🆕 Создан новый питомец!")
+	return NewPet(names)
 }
